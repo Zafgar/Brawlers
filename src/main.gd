09180157c0c814625ce36bca64f577d56c5e200c
Zafgar <tonikeskitalo91@gmail.com>
2026-07-22 16:00:34 +0300

    Delete src directory

--- src/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-## Pelin juurisolmu. Kaikki ruudut (valikot, areena, tulokset) elävät tämän lapsina.
-## Varsinainen tilanhallinta on Game-autoloadissa.
-
-
-func _ready() -> void:
-	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
-	Game.boot(self)
